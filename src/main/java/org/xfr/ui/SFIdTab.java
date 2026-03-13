8c56bdeeb64768c8f46fe15fc94ed1df1e0eeaf2
doberzan <doberzan42@gmail.com>
2026-03-13 12:08:04 -0500

    Fixed intruder bug

--- src/main/java/org/xfr/ui/SFIdTab.java
+++ src/main/java/org/xfr/ui/SFIdTab.java
@@ -1,5 +1,6 @@
 package org.xfr.ui;
 
+import burp.api.montoya.core.ToolType;
 import burp.api.montoya.http.handler.*;
 import burp.api.montoya.http.message.params.HttpParameterType;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -9,6 +10,9 @@ import org.xfr.SFScan;
 import javax.swing.*;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeNode;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -19,10 +23,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.TreeSet;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -31,17 +32,18 @@ import static burp.api.montoya.http.handler.RequestToBeSentAction.continueWith;
 public class SFIdTab extends JPanel implements ActionListener, HttpHandler {
     private final SFScan sfScan;
     private String currentContext;
-
     private JButton saveButton;
-
     private JTable idTable;
-
     private DefaultTableModel tableModel;
-
-    public TreeSet<String> sfIds;
+    private DefaultTreeModel treeModel;
+    private DefaultMutableTreeNode treeRoot;
+    private JTree tree;
+    private HashMap<String, String[]> treeData;
+    private TreeSet<String> sfIds;
 
     public SFIdTab(SFScan sfScan) {
         this.sfScan = sfScan;
+        this.treeData = new HashMap<>();
         this.sfIds = new TreeSet<>();
         this.currentContext = "";
         buildPanelUI();
@@ -50,11 +52,17 @@ public class SFIdTab extends JPanel implements ActionListener, HttpHandler {
     private void buildPanelUI() {
         this.setLayout(new BorderLayout());
 
-        saveButton = new JButton("Save Ids");
+        // Creating the root tree node
+        treeRoot = new DefaultMutableTreeNode("SF IDs (0)");
 
+        // Creating the JTree
+        tree = new JTree(treeRoot);
+        tree.setFont(new Font("Courier New", Font.PLAIN, 18));
+        treeModel = (DefaultTreeModel) tree.getModel();
+
+        saveButton = new JButton("Save Ids");
         JPanel buttonCollection = new JPanel();
         buttonCollection.setLayout(new GridLayout(1, 3));
-
         saveButton.setPreferredSize(new Dimension(100, 50));
 
         // Build panel
@@ -66,7 +74,7 @@ public class SFIdTab extends JPanel implements ActionListener, HttpHandler {
 
         // Add items to panel
         buttonCollection.add(saveButton);
-        this.add(scrollPane, BorderLayout.CENTER);
+        this.add(new JScrollPane(tree));
         this.add(buttonCollection, BorderLayout.SOUTH);
     }
 
@@ -97,19 +105,34 @@ public class SFIdTab extends JPanel implements ActionListener, HttpHandler {
         }
     }
 
-    private void addSFID(String id)
-    {
-        // This is the most disgusting function I have ever written, but it works sooo
-        sfIds.add(id);
-        String[] columnNames = {"Potential SF Ids ("+sfIds.size()+")"};
-        String[][] tmpTableData = new String[sfIds.size()][];
-        String[] tmp = sfIds.toArray(new String[0]);
-        for (int i = 0; i < sfIds.size(); i++)
+    private void addSFID(String id) {
+        String prefix = id.substring(0, 3);
+
+        // Check if ID is duplicate
+        if(sfIds.contains(id))
         {
-            tmpTableData[i] = new String[]{tmp[i]};
+            return;
         }
-        tableModel = new DefaultTableModel(tmpTableData, columnNames);
-        idTable.setModel(tableModel);
+
+        sfIds.add(id);
+
+        // Check if ID prefix does not exist
+        if(!treeData.containsKey(prefix))
+        {
+            ((DefaultMutableTreeNode)treeModel.getRoot()).add(new DefaultMutableTreeNode(prefix));
+            treeData.put(prefix,new String[0]);
+        }
+
+        // Update tree
+        Enumeration<TreeNode> children = ((DefaultMutableTreeNode)treeModel.getRoot()).children();
+        while (children.hasMoreElements()) {
+            DefaultMutableTreeNode parent = ((DefaultMutableTreeNode)children.nextElement());
+            // Check if parent prefix matches id prefix
+            if(parent.getUserObject().toString().equals(prefix))
+            {
+                parent.add(new DefaultMutableTreeNode(id));
+            }
+        }
     }
 
     private String[] checkSFID(String content)
@@ -119,7 +142,7 @@ public class SFIdTab extends JPanel implements ActionListener, HttpHandler {
         Pattern pattern = Pattern.compile("\\b[a-zA-Z0-9]{5}0[a-zA-Z0-9]{9}(?:[a-zA-Z0-9]{3})?\\b");
         Matcher matcher = pattern.matcher(content);
         while (matcher.find()) {
-            addSFID(matcher.group());
+            addSFID(matcher.group().strip());
         }
         return ids.toArray(new String[0]);
     }
@@ -128,7 +151,7 @@ public class SFIdTab extends JPanel implements ActionListener, HttpHandler {
     public RequestToBeSentAction handleHttpRequestToBeSent(HttpRequestToBeSent requestToBeSent) {
 
         // Collect message body for SF POST messages
-        if(requestToBeSent.method().equalsIgnoreCase("POST") && requestToBeSent.hasParameter("message", HttpParameterType.BODY) && requestToBeSent.hasParameter("aura.context", HttpParameterType.BODY))
+        if(requestToBeSent.isInScope() && requestToBeSent.toolSource().toolType() == ToolType.PROXY && requestToBeSent.method().equalsIgnoreCase("POST") && requestToBeSent.hasParameter("message", HttpParameterType.BODY) && requestToBeSent.hasParameter("aura.context", HttpParameterType.BODY))
         {
             String decoded = safeDecode(requestToBeSent.bodyToString());
             checkSFID(decoded);
@@ -139,7 +162,7 @@ public class SFIdTab extends JPanel implements ActionListener, HttpHandler {
     @Override
     public ResponseReceivedAction handleHttpResponseReceived(HttpResponseReceived responseReceived) {
         // Collect message body for responses that are in scope and are less than 1 MB
-        if(sfScan.api.scope().isInScope(responseReceived.initiatingRequest().url()) && responseReceived.body().length() < 1000000)
+        if(responseReceived.toolSource().toolType() == ToolType.PROXY && sfScan.api.scope().isInScope(responseReceived.initiatingRequest().url()) && responseReceived.body().length() < 1000000)
         {
             String decoded = safeDecode(responseReceived.bodyToString());
             checkSFID(decoded);
